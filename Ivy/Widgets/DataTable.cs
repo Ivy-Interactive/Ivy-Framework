4fe42bed500b2b97a66c9d0b8abc1426f0b457a0
rorychatt <pavel.rinne@gmail.com>
2025-07-15 11:21:44 +0200

    feat: correct formatting

--- Ivy/Widgets/DataTable.cs
+++ Ivy/Widgets/DataTable.cs
@@ -6,23 +6,23 @@ namespace Ivy;
 public record DataTable : WidgetBase<DataTable>
 {
     [Prop] public string? ServerUrl { get; set; }
-    
+
     [Prop] public string? Query { get; set; }
-    
+
     [Prop] public int Limit { get; set; } = 100;
-    
+
     [Prop] public int Offset { get; set; } = 0;
-    
+
     [Prop] public bool ShowQueryInput { get; set; } = true;
-    
+
     [Prop] public bool ShowRefreshButton { get; set; } = true;
-    
+
     [Prop] public bool ShowStatus { get; set; } = true;
-    
+
     [Prop] public string? Title { get; set; }
-    
+
     [Prop] public string? Description { get; set; }
-    
+
     [Prop] public bool ResizableColumns { get; set; } = false;
 }
 
@@ -32,49 +32,49 @@ public static class DataTableExtensions
     {
         return dataTable with { ServerUrl = serverUrl };
     }
-    
+
     public static DataTable Query(this DataTable dataTable, string query)
     {
         return dataTable with { Query = query };
     }
-    
+
     public static DataTable Limit(this DataTable dataTable, int limit)
     {
         return dataTable with { Limit = limit };
     }
-    
+
     public static DataTable Offset(this DataTable dataTable, int offset)
     {
         return dataTable with { Offset = offset };
     }
-    
+
     public static DataTable ShowQueryInput(this DataTable dataTable, bool show = true)
     {
         return dataTable with { ShowQueryInput = show };
     }
-    
+
     public static DataTable ShowRefreshButton(this DataTable dataTable, bool show = true)
     {
         return dataTable with { ShowRefreshButton = show };
     }
-    
+
     public static DataTable ShowStatus(this DataTable dataTable, bool show = true)
     {
         return dataTable with { ShowStatus = show };
     }
-    
+
     public static DataTable Title(this DataTable dataTable, string title)
     {
         return dataTable with { Title = title };
     }
-    
+
     public static DataTable Description(this DataTable dataTable, string description)
     {
         return dataTable with { Description = description };
     }
-    
+
     public static DataTable ResizableColumns(this DataTable dataTable, bool resizable = true)
     {
         return dataTable with { ResizableColumns = resizable };
     }
-} 
+}
