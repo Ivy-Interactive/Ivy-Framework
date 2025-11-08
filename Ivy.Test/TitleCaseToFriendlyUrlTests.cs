543028fefedf4200bb97d7ad1aff308f477c9580
Artem Khvorostianyi <ar.khvorostianyi@gmail.com>
2025-11-09 00:42:06 +0200

    feat: implement more test cases

--- Ivy.Test/TitleCaseToFriendlyUrlTests.cs
+++ Ivy.Test/TitleCaseToFriendlyUrlTests.cs
@@ -13,6 +13,9 @@ public class TitleCaseToFriendlyUrlTests
     [InlineData("_FooBarApp", "_foo-bar")] // leading underscore preserved
     [InlineData("_CLIApp", "_cli")] // leading underscore with acronym
     [InlineData("_CLI", "_cli")] // leading underscore acronym without App
+    [InlineData("HTMLParser", "html-parser")] // acronym followed by word
+    [InlineData("FOOBar", "foo-bar")] // multi-letter acronym followed by standard word
+    [InlineData("XMLHttpRequest", "xml-http-request")] // acronym followed by camel word then another
     public void ConvertsCorrectly(string input, string expected)
     {
         var result = Utils.TitleCaseToFriendlyUrl(input);
