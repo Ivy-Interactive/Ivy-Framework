70e71e8c0e63ed0ae0ce0e3d25a4dc8f087e8989
defymecobra <9micelondon@gmail.com>
2025-11-16 03:24:21 +0200

    docs: minify XML in Widgets 1

--- Ivy/Widgets/Effects/Confetti.cs
+++ Ivy/Widgets/Effects/Confetti.cs
@@ -3,35 +3,15 @@
 // ReSharper disable once CheckNamespace
 namespace Ivy;
 
-/// <summary>
-/// Widget that creates a celebratory confetti effect around its child content.
-/// </summary>
 public record Confetti : WidgetBase<Confetti>
 {
-    /// <summary>
-    /// Initializes a new instance of the Confetti class.
-    /// </summary>
-    /// <param name="child">
-    /// The child widget or content.
-    /// </param>
     public Confetti(object? child = null) : base(child != null ? [child] : [])
     {
     }
 
-    /// <summary>
-    /// Gets or sets when the confetti effect should be triggered.
-    /// </summary>
     [Prop] public AnimationTrigger Trigger { get; init; } = AnimationTrigger.Auto;
 
-    /// <summary>
-    /// Overrides the | operator to add a single child.
-    /// </summary>
-    /// <param name="widget">The Confetti widget.</param>
-    /// <param name="child">The single child widget or content to add.</param>
-    /// <returns>A new Confetti widget with the specified child.</returns>
-    /// <exception cref="NotSupportedException">
-    /// Thrown when attempting to add multiple children.
-    /// </exception>
+    /// <exception cref="NotSupportedException">Thrown when attempting to add multiple children.</exception>
     public static Confetti operator |(Confetti widget, object child)
     {
         if (child is IEnumerable<object> _)
