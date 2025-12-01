e188dafef82c84eafed15d930db3fadb2eae1bca
rorychatt <pavel.rinne@gmail.com>
2025-12-01 15:13:09 +0100

    bugfix: further reduce content left padding to align with header text

--- frontend/src/components/ui/expandable/expandable-variants.ts
+++ frontend/src/components/ui/expandable/expandable-variants.ts
@@ -67,9 +67,9 @@ export const expandableChevronVariants = cva(
 export const expandableContentVariants = cva('overflow-hidden transition-all', {
   variants: {
     scale: {
-      Small: 'pl-2 pr-2 py-2 space-y-2 [&_*]:text-xs',
-      Medium: 'pl-2 pr-3 py-4 space-y-4 [&_*]:text-sm',
-      Large: 'pl-2 pr-4 py-6 space-y-5 [&_*]:text-base',
+      Small: 'pl-1 pr-2 py-2 space-y-2 [&_*]:text-xs',
+      Medium: 'pl-1 pr-3 py-4 space-y-4 [&_*]:text-sm',
+      Large: 'pl-1.5 pr-4 py-6 space-y-5 [&_*]:text-base',
     },
   },
   defaultVariants: {
