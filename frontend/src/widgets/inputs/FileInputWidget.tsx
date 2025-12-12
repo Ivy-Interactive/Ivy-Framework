ccd66996635108a9cfa6b0d96dc679c42c397c0c
defymecobra <9micelondon@gmail.com>
2025-12-13 00:07:07 +0200

    [InvalidIcon]: move pointer-events-auto to component itself

--- frontend/src/widgets/inputs/FileInputWidget.tsx
+++ frontend/src/widgets/inputs/FileInputWidget.tsx
@@ -381,9 +381,7 @@ export const FileInputWidget: React.FC<FileInputWidgetProps> = ({
       {/* Invalid icon in top right corner - only for required field validation */}
       {invalid && (
         <div className="absolute top-2 right-2 z-20 pointer-events-none">
-          <div className="pointer-events-auto">
-            <InvalidIcon message={invalid} />
-          </div>
+          <InvalidIcon message={invalid} />
         </div>
       )}
       <div
