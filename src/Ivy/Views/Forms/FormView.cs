3654a9600dc68f702921e9a26da881b4d90c078e
rorychatt <pavel.rinne@gmail.com>
2026-01-14 20:15:36 +0100

    chore: forms lexs waste of y space

--- src/Ivy/Views/Forms/FormView.cs
+++ src/Ivy/Views/Forms/FormView.cs
@@ -193,9 +193,10 @@ public class FormView<TModel>(IFormFieldView[] fieldViews, Func<Event<Form>, Val
         {
             var gap = scale switch
             {
-                Scale.Small => 4,
-                Scale.Medium => 6,
-                _ => 8
+                Scale.Medium => 3,
+                Scale.Small => 2,
+                Scale.Large => 4,
+                _ => 2
             };
 
             return Layout
