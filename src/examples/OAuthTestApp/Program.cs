d1a22241ae66422a75a5d24b6929f43efc4fcd09
defymecobra <9micelondon@gmail.com>
2026-02-05 16:55:37 +0200

    chore: remove commentaries

--- src/examples/OAuthTestApp/Program.cs
+++ src/examples/OAuthTestApp/Program.cs
@@ -1,13 +1,10 @@
 using Ivy;
 using Ivy.Auth.GitHub;
 
-// Создаём сервер
 var server = new Server();
 
-// Включаем Hot Reload для разработки
 server.UseHotReload();
 
-// Регистрируем HttpClient для GitHub API
 server.Services.AddHttpClient("GitHubAuth", client =>
 {
     client.DefaultRequestHeaders.Add("User-Agent", "Ivy-Framework");
@@ -15,20 +12,14 @@ server.Services.AddHttpClient("GitHubAuth", client =>
     client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
 });
 
-// Регистрируем IConfiguration
 server.Services.AddSingleton(server.Configuration);
 
-// Настраиваем GitHub Auth Provider
 server.UseAuth<GitHubAuthProvider>(c => c.UseGitHub());
 
-// Добавляем приложения из сборки
 server.AddAppsFromAssembly();
 
-// Настраиваем Chrome (боковая панель)
 server.UseChrome();
 
-// Устанавливаем заголовок
 server.SetMetaTitle("OAuth Test App");
 
-// Запускаем сервер
 await server.RunAsync();
