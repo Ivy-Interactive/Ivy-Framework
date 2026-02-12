00a61e0f91ffde01e550ee1356e9103f7e6b453b
Artem Khvorostianyi <ar.khvorostianyi@gmail.com>
2026-02-12 13:15:25 +0200

    feat: use better structure for text area element

--- src/frontend/src/components/ui/textarea/textarea.tsx
+++ src/frontend/src/components/ui/textarea/textarea.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
+import './textarea.css';
+
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
   React.ComponentProps<'textarea'>
