8959fc9b10f14c743dbc46f2a624408cd2746afa
nielsbosma <niels@bosmainteractive.se>
2026-03-25 14:45:06 +0000

    Add New Plan button to AppShell footer
    - Added NewPlanFooterButton view component for navigation context
    - Button renders above settings dropdown as standalone footer element
    - Uses Outline variant with Plus icon for consistent design
    - Provides quick access to plan creation from any app

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using Ivy;
 using Microsoft.Extensions.DependencyInjection;
 using Ivy.Tendril;
+using Ivy.Tendril.Apps;
 using Ivy.Tendril.Services;
+
 var server = new Server();
 server.UseCulture("en-US");
 #if DEBUG
@@ -13,5 +15,20 @@ server.Services.AddSingleton<PlanReaderService>();
 server.Services.AddSingleton<TaskService>();
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
-server.UseAppShell(new AppShellSettings().UseTabs(preventDuplicates: true));
+server.UseAppShell(new AppShellSettings()
+    .UseTabs(preventDuplicates: true)
+    .Footer(new NewPlanFooterButton()));
 await server.RunAsync();
+
+public class NewPlanFooterButton : ViewBase
+{
+    public override object? Build()
+    {
+        var navigator = UseNavigation();
+        return new Button("New Plan")
+            .Icon(Icons.Plus)
+            .Width(Size.Full())
+            .Variant(ButtonVariant.Outline)
+            .OnClick(() => navigator.Navigate<PlansApp>());
+    }
+}
