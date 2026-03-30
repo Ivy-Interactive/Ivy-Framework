d4da580aa0d80fd3cd31710584818fe3eb4a0dd3
Zach Wolfe <zachrwolfe@me.com>
2026-03-30 16:37:25 -0500

    Fix formatting

--- src/frontend/vite.config.mjs
+++ src/frontend/vite.config.mjs
@@ -66,11 +66,13 @@ const injectMeta = (mode) => {
 const mode = process.env.NODE_ENV || "development";
 function insecureFetch(url) {
   return new Promise((resolve, reject) => {
-    https.get(url, { rejectUnauthorized: false }, (res) => {
-      let data = "";
-      res.on("data", (chunk) => (data += chunk));
-      res.on("end", () => resolve(data));
-    }).on("error", reject);
+    https
+      .get(url, { rejectUnauthorized: false }, (res) => {
+        let data = "";
+        res.on("data", (chunk) => (data += chunk));
+        res.on("end", () => resolve(data));
+      })
+      .on("error", reject);
   });
 }
 
