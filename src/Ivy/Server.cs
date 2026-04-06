c82dfa1c73bcb6d4086e72a05780be75c8e4a62a
rorychatt <pavel.rinne@gmail.com>
2026-04-06 12:11:27 +0200

    Fix merge conflict errors inside Server.cs

--- src/Ivy/Server.cs
+++ src/Ivy/Server.cs
@@ -732,7 +732,7 @@ public class Server
         app.MapGrpcService<DataTableService>().EnableGrpcWeb();
 
         app.UseFrontend(_args, logger2);
-        app.UseAssets(_args, logger2, "Assets", "ivy/assets");
+        app.UseAssets(_args, logger2, "Assets", "ivy-assets");
 
         return app;
     }
@@ -866,10 +866,7 @@ public class Server
                 }
             };
         }
-      
-        app.UseFrontend(_args, logger);
-        app.UseAssets(_args, logger, "Assets", "ivy-assets");
-      
+
         app.Lifetime.ApplicationStarted.Register(() =>
         {
             var url = app.Urls.FirstOrDefault() ?? "unknown";
