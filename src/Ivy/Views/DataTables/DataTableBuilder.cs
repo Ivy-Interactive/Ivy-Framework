3c58e7246732ba6a99d49816dc89ad1f30dab1c6
Artem Khvorostianyi <ar.khvorostianyi@gmail.com>
2026-04-09 15:40:25 +0300

    feat: remove custom color props and handle with native color api

--- src/Ivy/Views/DataTables/DataTableBuilder.cs
+++ src/Ivy/Views/DataTables/DataTableBuilder.cs
@@ -329,7 +329,6 @@ public class DataTableBuilder<TModel>(
         if (renderer is LabelsDisplayRenderer labelsRenderer)
         {
             column.Column.Color = labelsRenderer.Color;
-            column.Column.CustomColor = labelsRenderer.CustomColor;
             column.Column.BadgeColorMapping = labelsRenderer.BadgeColorMapping;
         }
 
@@ -341,14 +340,6 @@ public class DataTableBuilder<TModel>(
         var column = GetColumn(field);
         column.Column.ColType = ColType.Labels;
         column.Column.Color = color;
-        return this;
-    }
-
-    public DataTableBuilder<TModel> Badges(Expression<Func<TModel, object>> field, string customColor)
-    {
-        var column = GetColumn(field);
-        column.Column.ColType = ColType.Labels;
-        column.Column.CustomColor = customColor;
         return this;
     }
 
