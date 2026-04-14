8de19e2fdb6af06577a06cc406e705e2ab8b930a
rorychatt <pavel.rinne@gmail.com>
2026-04-14 10:23:53 +0200

    [00178] Fix frontend formatting

--- src/frontend/src/widgets/inputs/DateTimeInputWidget/DateTimeInputWidget.tsx
+++ src/frontend/src/widgets/inputs/DateTimeInputWidget/DateTimeInputWidget.tsx
@@ -173,7 +173,13 @@ export const DateTimeInputWidget: React.FC<DateTimeInputWidgetProps> = ({
           {prefixContent}
         </div>
       )}
-      <div className={cn("flex-1 relative w-full [&_button]:border-0 [&_button]:shadow-none [&_input]:border-0 [&_input]:shadow-none", hasPrefix && "[&_button]:rounded-l-none [&_input]:rounded-l-none", hasSuffix && "[&_button]:rounded-r-none [&_input]:rounded-r-none")}>
+      <div
+        className={cn(
+          "flex-1 relative w-full [&_button]:border-0 [&_button]:shadow-none [&_input]:border-0 [&_input]:shadow-none",
+          hasPrefix && "[&_button]:rounded-l-none [&_input]:rounded-l-none",
+          hasSuffix && "[&_button]:rounded-r-none [&_input]:rounded-r-none",
+        )}
+      >
         {variantElement}
       </div>
       {hasSuffix && (
