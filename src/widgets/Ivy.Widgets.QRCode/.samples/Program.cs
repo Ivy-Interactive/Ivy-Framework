5c74da28b4137e8fb896ef47040a456de552d2c6
Christopher Clemons <chris.clemons85@gmail.com>
2026-05-13 13:11:17 +0200

    add basic usage example

--- src/widgets/Ivy.Widgets.QRCode/.samples/Program.cs
+++ src/widgets/Ivy.Widgets.QRCode/.samples/Program.cs
@@ -2,21 +2,40 @@ using Ivy;
 using Ivy.Widgets.QRCode;
 
 var server = new Server();
-server.AddApp<QRCodeApp>();
+server.AddApp<QRCodeDemo>();
 await server.RunAsync();
 
 [App]
-class QRCodeApp : ViewBase
+class QRCodeDemo : ViewBase
 {
     public override object Build()
     {
         var state = UseState("https://tendril.ivy.app/");
         return Layout.Vertical().Width(Size.Fit())
+            | Text.H1("QRCode")
+            | Text.H2("Basic Usage")
+            | new CodeBlock(@$"class QRCodeDemo : ViewBase
+{{
+    public override object Build()
+    {{
+        var state = UseState({"\"https://tendril.ivy.app/\""});
+        return Layout.Vertical()
             | new QRCode()
                 .Value(state.Value)
                 .PixelSize(256)
                 .Foreground(Colors.Primary)
                 .Background(Colors.White)
             | state.ToTextInput();
+    }}
+}}")
+            | (Layout.Vertical()
+                | new QRCode()
+                    .Value(state.Value)
+                    .PixelSize(256)
+                    .Foreground(Colors.Primary)
+                    .Background(Colors.White)
+                    .AlignSelf(Align.Center)
+                | state.ToTextInput())
+            .WithBox();
     }
 }
