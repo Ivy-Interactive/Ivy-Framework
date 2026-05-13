96994d5754529a515e2c4cd7c28aef239e6180a9
Christopher Clemons <chris.clemons85@gmail.com>
2026-05-13 17:08:09 +0200

    add full props usage example in QRCodeDemo sample app

--- src/widgets/Ivy.Widgets.QRCode/.samples/Program.cs
+++ src/widgets/Ivy.Widgets.QRCode/.samples/Program.cs
@@ -11,26 +11,70 @@ class QRCodeDemo : ViewBase
     public override object Build()
     {
         var client = UseService<IClientProvider>();
-        var state = UseState("https://tendril.ivy.app/");
-        return Layout.Vertical().Width(Size.Fit())
-            | Text.H1("QRCode")
-            | Text.H2("Basic Usage")
-            | new CodeBlock(@$"class QRCodeDemo : ViewBase
+        var basicExampleQrContent = UseState("https://tendril.ivy.app/");
+        var fullExampleContent = UseState("https://tendril.ivy.app/");
+        var pixelSize = UseState(300);
+        var foregroundColor = UseState(Colors.Emerald);
+        var backgroundColor = UseState(Colors.White);
+        var includeMargin = UseState(false);
+        var errorCorrectionLevel = UseState(QrErrorCorrectionLevel.Low);
+
+        var fullExampleToolbar = Layout.Horizontal().Gap(2).Width(Size.Fit())
+            | foregroundColor.ToColorInput().Variant(ColorInputVariant.SwatchPicker).WithLabel("Foreground")
+            | backgroundColor.ToColorInput().Variant(ColorInputVariant.SwatchPicker).WithLabel("Background")
+            | pixelSize.ToNumberInput().Min(100).Max(1000).WithLabel("PixelSize")
+            | includeMargin.ToBoolInput().WithLabel("IncludeMargin")
+            | errorCorrectionLevel.ToSelectInput().WithLabel("ErrorCorrectionLevel");
+
+        return Layout.Vertical().Gap(16).Width(Size.Auto())
+            | (Layout.Vertical()
+                | Text.H1("QRCode")
+                | Text.H2("Basic Usage")
+                | new CodeBlock(@$"class QRCodeBasicDemo : ViewBase
 {{
     public override object Build()
     {{
-        var state = UseState({"\"https://tendril.ivy.app/\""});
+        var basicExampleQrContent = UseState({"\"https://tendril.ivy.app/\""});
+
         return Layout.Vertical()
-            | new QRCode().Value(state.Value)
-            | state.ToTextInput();
+            | new QRCode().Value(basicExampleQrContent.Value)
+            | basicExampleQrContent.ToTextInput();
     }}
 }}")
+                | (Layout.Vertical()
+                    | new QRCode()
+                        .Value(basicExampleQrContent.Value)
+                        .AlignSelf(Align.Center)
+                    | basicExampleQrContent.ToTextInput())
+                .WithBox())
+
             | (Layout.Vertical()
-                | new QRCode()
-                    .Value(state.Value)
-                    .AlignSelf(Align.Center)
-                | state.ToTextInput())
-            .WithBox()
+                | Text.H2("Full Props Usage")
+                | new CodeBlock(@$"class QRCodeFullDemo : ViewBase
+{{
+    public override object Build()
+    {{
+        return new QRCode()
+            .Value({$"\"{fullExampleContent.Value}\""})
+            .PixelSize({pixelSize.Value})
+            .Foreground(Colors.{foregroundColor.Value})
+            .Background(Colors.{backgroundColor.Value})
+            .IncludeMargin({includeMargin.Value.ToString().ToLower()})
+            .ErrorCorrectionLevel(QrErrorCorrectionLevel.{errorCorrectionLevel.Value});
+    }}
+}}")
+                | (Layout.Vertical()
+                    | new QRCode()
+                        .Value(fullExampleContent.Value)
+                        .PixelSize(pixelSize.Value)
+                        .Foreground(foregroundColor.Value)
+                        .Background(backgroundColor.Value)
+                        .IncludeMargin(includeMargin.Value)
+                        .ErrorCorrectionLevel(errorCorrectionLevel.Value)
+                        .AlignSelf(Align.Center)
+                    | fullExampleContent.ToTextInput()
+                    | fullExampleToolbar)
+                .WithBox())
 
             | new DropDownMenu(@evt =>
                 {
