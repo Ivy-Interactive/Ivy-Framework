892c6ab3e1ae2b3c2891d37e7344d60af537896a
Zach Wolfe <zachrwolfe@me.com>
2026-05-14 03:54:08 -0500

    Add BuildConfigurationView method to IIvyPlugin

--- src/Ivy.Plugin.Abstractions/IIvyPlugin.cs
+++ src/Ivy.Plugin.Abstractions/IIvyPlugin.cs
@@ -5,4 +5,5 @@ public interface IIvyPlugin
     PluginManifest Manifest { get; }
     PluginConfigurationSchema? ConfigurationSchema { get; }
     void Configure(IIvyPluginContext context);
+    object? BuildConfigurationView(IPluginConfigWriter configWriter) => null;
 }
