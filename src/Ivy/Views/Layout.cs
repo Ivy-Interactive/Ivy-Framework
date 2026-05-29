caf5d383562582f763d88ff8c976bc61c8ec0588
defymecobra <9micelondon@gmail.com>
2026-05-29 21:10:35 +0300

    [Layout]: Fix greedy flex behavior in Spacer and margin wrappers

--- src/Ivy/Views/Layout.cs
+++ src/Ivy/Views/Layout.cs
@@ -69,17 +69,17 @@ public static class LayoutExtensions
 {
     public static LayoutView WithMargin(this object anything, int margin)
     {
-        return Layout.Horizontal(anything).Margin(margin);
+        return new LayoutView().Horizontal(anything).Margin(margin);
     }
 
     public static LayoutView WithMargin(this object anything, int marginX, int marginY)
     {
-        return Layout.Horizontal(anything).Margin(marginX, marginY);
+        return new LayoutView().Horizontal(anything).Margin(marginX, marginY);
     }
 
     public static LayoutView WithMargin(this object anything, int left, int top, int right, int bottom)
     {
-        return Layout.Horizontal(anything).Margin(left, top, right, bottom);
+        return new LayoutView().Horizontal(anything).Margin(left, top, right, bottom);
     }
 
     public static LayoutView WithLayout(this object anything)
