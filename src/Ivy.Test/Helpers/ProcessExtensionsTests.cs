5e3b8737433885c0210a90845bf31c327ccd9107
rorychatt <pavel.rinne@gmail.com>
2026-08-15 17:43:48 +0200

    test: add unit test verifying prompt return when child process holds inherited pipe open

--- src/Ivy.Test/Helpers/ProcessExtensionsTests.cs
+++ src/Ivy.Test/Helpers/ProcessExtensionsTests.cs
@@ -184,4 +184,38 @@ public class ProcessExtensionsTests
         Assert.Equal(0, process.ExitCode);
         Assert.True(sw.Elapsed < TimeSpan.FromSeconds(5));
     }
+
+    [Fact]
+    public async Task WaitForExitOrKillAsync_WhenChildHoldsRedirectedPipes_ReturnsPromptlyAfterParentExits()
+    {
+        if (OperatingSystem.IsWindows())
+            return;
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = "/bin/sh",
+            Arguments = "-c \"sleep 10 >&1 & exit 0\"",
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+        };
+
+        using var process = new Process { StartInfo = psi };
+        process.OutputDataReceived += (_, _) => { };
+        process.ErrorDataReceived += (_, _) => { };
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        var sw = Stopwatch.StartNew();
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        var result = await process.WaitForExitOrKillAsync(cts.Token);
+        sw.Stop();
+
+        Assert.True(result);
+        Assert.True(process.HasExited);
+        Assert.True(sw.Elapsed < TimeSpan.FromSeconds(3),
+            $"Should exit promptly without waiting for background child to close pipe, took {sw.Elapsed.TotalSeconds}s");
+    }
 }
